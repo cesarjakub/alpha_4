f3b0789ebbc4b835d704b570aa4640b8fccac968
cesarjakub <cesar@spsejecna.cz>
2024-02-23 18:24:56 +0100

    feat: udp and tcp

--- test/udp_discovery.py
+++ test/udp_discovery.py
@@ -1,6 +1,7 @@
 import socket
 import time
 import json
+from tcp import TCPProtocol
 from datetime import datetime
 from threading import Thread
 
@@ -9,9 +10,6 @@ class UDPDiscovery:
         self.peer_id = peer_id
         self.broadcast_ip = broadcast_ip
         self.broadcast_port = broadcast_port
-
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
         self.running = False
 
@@ -22,37 +20,39 @@ class UDPDiscovery:
 
     def stop(self):
         self.running = False
-        self.socket.close()
 
     def _run_discovery(self):
-        try:
-            while self.running:
-                message = {"command": "hello", "peer_id": self.peer_id}
-                encoded_message = json.dumps(message).encode("utf-8")
-                self.socket.sendto(encoded_message, (self.broadcast_ip, self.broadcast_port))
-                self.socket.settimeout(5)
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as self.socket:
+            self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+            try:
+                while self.running:
+                    message = {"command": "hello", "peer_id": self.peer_id}
+                    encoded_message = json.dumps(message).encode("utf-8")
+                    self.socket.sendto(encoded_message, (self.broadcast_ip, self.broadcast_port))
+                    self.socket.settimeout(5)
 
-                while True:
-                    try:
-                        data, addr = self.socket.recvfrom(1024)
-                        response = json.loads(data.decode("utf-8"))
+                    while True:
+                        try:
+                            data, addr = self.socket.recvfrom(1024)
+                            response = json.loads(data.decode("utf-8"))
 
-                        if response.get("command") == "hello":
-                            my_response = {"status": "ok", "peer_id": self.peer_id}
-                            encoded_response = json.dumps(my_response).encode("utf-8")
-                            self.socket.sendto(encoded_response, addr)
-                            print(f"{datetime.now().strftime('%b %d %H:%M:%S')} {self.peer_id}: Server: Sent response to the remote {addr[0]}:{addr[1]} - {my_response}")
+                            if response.get("command") == "hello":
+                                my_response = {"status": "ok", "peer_id": self.peer_id}
+                                encoded_response = json.dumps(my_response).encode("utf-8")
+                                self.socket.sendto(encoded_response, addr)
+                                print(f"{datetime.now().strftime('%b %d %H:%M:%S')} {self.peer_id}: Server: Sent response to the remote {addr[0]}:{addr[1]} - {my_response}")
 
-                        if response.get("status") == "ok" and response.get("peer_id") != self.peer_id:
-                            print(f"{datetime.now().strftime('%b %d %H:%M:%S')} {self.peer_id}: UDPDiscovery: Server: Received request {self.broadcast_ip} from the remote {addr[0]}:{addr[1]} - {response}")
-                    except socket.timeout:
-                        break
-            time.sleep(5)
-        except KeyboardInterrupt:
-            print("Something went wrong")
-        finally:
-            self.socket.close()
-            print("Socket closed.")
+                            if response.get("status") == "ok" and response.get("peer_id") != self.peer_id:
+                                tcp = TCPProtocol(self.peer_id)
+                                tcp.establish_connectio(addr[0])
+                                print(f"{datetime.now().strftime('%b %d %H:%M:%S')} {self.peer_id}: UDPDiscovery: Server: Received request {self.broadcast_ip} from the remote {addr[0]}:{addr[1]} - {response}")
+                        except socket.timeout:
+                            break
+                time.sleep(5)
+            except KeyboardInterrupt:
+                print("Something went wrong")
+            finally:
+                print("Socket closed.")
 
 
 
