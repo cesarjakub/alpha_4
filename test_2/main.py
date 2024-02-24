87c3c47f7f107e305c1668bdbdac8e1e60c7a32e
cesarjakub <cesar@spsejecna.cz>
2024-02-24 13:35:16 +0100

    feat: udp fix bug

--- test_2/main.py
+++ test_2/main.py
@@ -13,18 +13,7 @@ def main():
         return
     """
     udp_tcp = HandleUDPandTCP("cesar-peer", "172.31.255.255", 9876)
-
-    udp_thread_one = threading.Thread(target=udp_tcp.udp_discovery)
-
-    try:
-        udp_thread_one.daemon = True
-        udp_thread_one.start()
-
-        udp_tcp.listen_udp()
-    except KeyboardInterrupt:
-        udp_thread_one.join()
-        print("Something went wrong with Threads")
-
+    udp_tcp.start_udp()
 
 if __name__ == '__main__':
     main()
--- test_2/udp_tcp.py
+++ test_2/udp_tcp.py
@@ -10,9 +10,9 @@ class HandleUDPandTCP:
         self.peer_id = peer_id
         self.broadcast = broadcast
         self.port = port
-        self.lock = threading.Lock()
         self.messages = set()
 
+    # udp handling
     def udp_discovery(self):
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp:
             udp.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
@@ -23,36 +23,56 @@ class HandleUDPandTCP:
                     udp.sendto(encoded_message, (self.broadcast, self.port))
                     print(f"{datetime.now().strftime('%b %d %H:%M:%S')} {self.peer_id}: UDPDiscovery: Send message {message}")
 
-                    time.sleep(5)
+                    time.sleep(2)
             except Exception as e:
                 print("DISCOVERY Something went wrong")
             finally:
                 udp.close()
 
-    def send_udp_response(self, data, addr):
-        with self.lock:
-            try:
-                res = json.loads(data.decode("utf-8"))
-                if res.get("command") == "hello" and res.get("peer_id") != self.peer_id:
-                    print(f"{datetime.now().strftime('%b %d %H:%M:%S')} {self.peer_id}: UDPDiscovery: Server: Received request {self.broadcast} from the remote {addr[0]}:{addr[1]} - {res}")
+    def send_udp_response(self, udp):
+        while True:
+            data, addr = udp.recvfrom(1024)
+            res = json.loads(data.decode("utf-8"))
+            if res.get("command") == "hello" and res.get("peer_id") != self.peer_id:
+                print(f"{datetime.now().strftime('%b %d %H:%M:%S')} {self.peer_id}: UDPDiscovery: Server: Received request {self.broadcast} from the remote {addr[0]}:{addr[1]} - {res}")
 
-                    udp_msg = {"status": "ok", "peer_id": self.peer_id}
-                    encoded_response = (json.dumps(udp_msg) + "\n").encode("utf-8")
-                    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as udp:
-                        udp.connect((addr[0], self.port))
-                        udp.sendall(encoded_response)
-                        self.tcp_handshake()
-                    print(f"{datetime.now().strftime('%b %d %H:%M:%S')} {self.peer_id}: UDPDiscovery: Send response to the remote {addr[0]}:{addr[1]} - {udp_msg}")
+                udp_msg = {"status": "ok", "peer_id": self.peer_id}
+                encoded_response = (json.dumps(udp_msg) + "\n").encode("utf-8")
+                with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_two:
+                    udp_two.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                    udp_two.connect(addr)
+                    udp_two.sendall(encoded_response)
+                    #self.tcp_handshake(addr)
+                print(f"{datetime.now().strftime('%b %d %H:%M:%S')} {self.peer_id}: UDPDiscovery: Send response to the remote {addr[0]}:{addr[1]} - {udp_msg}")
+
+
+    def listen(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp:
+            udp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            try:
+                udp.bind(('0.0.0.0', self.port))
+                self.send_udp_response(udp)
             except Exception as e:
                 print("SEND Something went wrong")
 
-    def listen_udp(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp:
-            udp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            udp.bind(('0.0.0.0', self.port))
-            while True:
-                data, addr = udp.recvfrom(1024)
-                threading.Thread(target=self.send_udp_response, args=(data, addr)).start()
+    def start_udp(self):
+        udp_thread_one = threading.Thread(target=self.udp_discovery)
+        udp_thread_two = threading.Thread(target=self.listen)
+        try:
+            udp_thread_one.start()
+            udp_thread_two.start()
+        except KeyboardInterrupt:
+            udp_thread_one.join()
+            udp_thread_two.join()
+            print("Something went wrong with Threads")
 
-    def tcp_handshake(self):
-        pass
+    # tcp handling
+    def tcp_handshake(self, addr):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp:
+            try:
+                tcp.connect(addr)
+                tcp_msg = {"command": "hello", "peer_id": self.peer_id}
+
+            except Exception as e:
+                print(f"Error during TCP handshake with {addr[0]}:{addr[1]}")
+
